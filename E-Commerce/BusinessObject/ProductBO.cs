7ccc7659f4485614528f3329a13a3a647af97c13
Michael Essam <Michael.Essam@venicecom.it>
2025-11-18 19:39:40 +0200

    Creating Products Controller and BO

---
+++ E-Commerce/BusinessObject/ProductBO.cs
@@ -0,0 +1,82 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Domain.Entities;
+using System.Text.Json.Nodes;
+
+namespace E_Commerce.BusinessObject
+{
+    public class ProductBO
+    {
+        private readonly AppDbContext _context;
+        public ProductBO(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void GetCreated(JsonObject product)
+        {
+            var ProductCreate = new Product();
+            ProductCreate.Name = product["name"]?.GetValue<string>();
+            ProductCreate.Description = product["description"]?.GetValue<string>();
+            ProductCreate.Price = product["price"]?.GetValue<decimal>() ?? 0;
+            ProductCreate.Quantity = product["quantity"]?.GetValue<int>() ?? 0;
+            ProductCreate.InWarranty = product["inwarranty"]?.GetValue<bool>() ?? false;
+            ProductCreate.WarrantyDescription = product["warrantydescription"]?.GetValue<string>();
+            ProductCreate.WarrantyMaxDate = product["warrantymmaxdate"]?.GetValue<DateOnly>() ?? DateOnly.MinValue;
+            ProductCreate.CategoryId = product["categoryid"]?.GetValue<int>() ?? 0;
+            ProductCreate.BrandId = product["brandid"]?.GetValue<int>() ?? 0;
+
+
+            //adding to db
+            _context.Products.Add(ProductCreate);
+            _context.SaveChanges();
+        }
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>();
+            products = _context.Products.ToList();
+            return products;
+        }
+        public Domain.Entities.Product GetProductById(JsonObject id)
+        {
+            int idproduct = id["name"].GetValue<int>();
+            var product = _context.Products.Where(a => a.Id == idproduct).FirstOrDefault();
+            return product;
+        }
+        public void GetUpdated(JsonObject product)
+        {
+            var findproduct = new Domain.Entities.Product();
+            findproduct.Id = product["id"].GetValue<int>();
+            var ProductUpdate = _context.Products.Where(a => a.Id == findproduct.Id).FirstOrDefault();
+            ProductUpdate.Name = product["name"]?.GetValue<string>();
+            ProductUpdate.Description = product["description"]?.GetValue<string>();
+            ProductUpdate.Price = product["price"]?.GetValue<decimal>() ?? 0;
+            ProductUpdate.Quantity = product["quantity"]?.GetValue<int>() ?? 0;
+            ProductUpdate.InWarranty = product["inwarranty"]?.GetValue<bool>() ?? false;
+            ProductUpdate.WarrantyDescription = product["warrantydescription"]?.GetValue<string>();
+            ProductUpdate.WarrantyMaxDate = product["warrantymmaxdate"]?.GetValue<DateOnly>() ?? DateOnly.MinValue;
+            ProductUpdate.CategoryId = product["categoryid"]?.GetValue<int>() ?? 0;
+            ProductUpdate.BrandId = product["brandid"]?.GetValue<int>() ?? 0;
+
+            //adding to db
+            _context.Products.Update(ProductUpdate);
+            _context.SaveChanges();
+        }
+
+        public bool GetDeleted(JsonObject idjson)
+        {
+            var id = idjson["id"].GetValue<int>();
+
+            var Product = _context.Products.Find(id);
+
+            if (Product != null)
+            {
+                _context.Products.Remove(Product);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- E-Commerce/Controllers/ProductsController.cs
+++ E-Commerce/Controllers/ProductsController.cs
@@ -1,11 +1,79 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using E_Commerce.BusinessObject;
+using E_Commerce.Domain.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
 
 namespace E_Commerce.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ProductBO _productBO;
+
+        public ProductsController(ProductBO productBO)
+        {
+            _productBO = productBO;
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(JsonObject product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _productBO.GetCreated(product);
+                return Ok();
+            }
+        }
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            var Products = _productBO.GetProducts();
+            return Products;
+        }
+        [HttpPost]
+        public IActionResult GetProduct(JsonObject productid)
+        {
+            if (productid == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var product = _productBO.GetProductById(productid);
+                return Ok(product);
+            }
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(JsonObject productid)
+        {
+            if (productid == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _productBO.GetUpdated(productid);
+                return Ok();
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(JsonObject productid)
+        {
+            if (productid == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _productBO.GetDeleted(productid);
+                return Ok();
+            }
+        }
     }
 }
